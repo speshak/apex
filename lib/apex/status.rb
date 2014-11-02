af3ccfe7599316b20180482bb2279fc81b1641a3
Scott Peshak <scott@peshak.net>
2014-11-02 16:18:51 -0600

    Parse probe info from status xml

--- lib/apex/status.rb
+++ lib/apex/status.rb
@@ -1,3 +1,6 @@
+require 'bigdecimal'
+require "nokogiri"
+
 module Apex
 	class Status
 		attr_accessor :date, :power, :probes, :outlets
@@ -9,7 +12,7 @@ module Apex
 		end
 
 
-		def initalize
+		def initialize
 			@probes = {}
 			@outlets = {}
 		end
@@ -19,6 +22,14 @@ module Apex
 			doc = Nokogiri::XML(xml)
 
 			@date = Date.strptime(doc.at_xpath('//status/date').text, '%m/%d/%Y %H:%M:%s')
+
+			doc.at_xpath('//status/probes').xpath('./probe').each{|probe|
+				add_probe(
+					probe.at_xpath('./name').text,
+					BigDecimal.new(probe.at_xpath('./value').text),
+					probe.at_xpath('./type').text
+				)
+			}
 		end
 
 
--- spec/lib/status_spec.rb
+++ spec/lib/status_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'bigdecimal'
 
 describe Apex::Status, '#read_xml' do
 	it "reads xml data into structures" do
@@ -6,5 +7,7 @@ describe Apex::Status, '#read_xml' do
 		status.read_xml(File.open(File.expand_path('../../../test-data/status.xml', __FILE__)))
 
 		expect(status.date).to eq(Date.strptime('12/29/2013 19:14:58', '%m/%d/%Y %H:%M:%s'))
+
+		expect(status.probes['Temp'][:value]).to eq(BigDecimal.new('77.6'))
 	end
 end
