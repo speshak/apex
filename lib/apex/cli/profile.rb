59d7b7e4f74ffd0af85f053f81c4131aa4bd0649
Scott Peshak <scott@peshak.net>
2016-01-20 14:52:48 -0600

    Add import subcommand to profile

--- lib/apex/cli/profile.rb
+++ lib/apex/cli/profile.rb
@@ -5,7 +5,13 @@ module Apex
     class ProfileCli < Thor
       desc "export <name>", "Exports profile named <name>"
       def export(name)
+        puts "Not yet implemented"
+      end
 
+
+      desc "import <name>", "Imports a profile named <name>.  Overwrites existing profile of the same name."
+      def import(name)
+        puts "Not yet implemented"
       end
     end
   end
