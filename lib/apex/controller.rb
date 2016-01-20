41ee21a1aa7364136401ad9b7a04b59fa35d9734
Scott Peshak <scott@peshak.net>
2016-01-20 15:34:03 -0600

    Add UserAgent creation

--- lib/apex/controller.rb
+++ lib/apex/controller.rb
@@ -1,8 +1,16 @@
-require "open-uri"
+require 'open-uri'
+require 'mechanize'
 
 module Apex
   class Controller 
     attr_accessor :url
+
+    # Controller username
+    attr_accessor :user
+
+    # Controller password
+    attr_accessor :password
+
 
     def initialize(url)
       @url = url
@@ -16,5 +24,17 @@ module Apex
     def datalog
       Datalog.from_xml(open("#{@url}/cgi-bin/datalog.xml"))
     end
+
+
+    # Get a prepared UserAgent
+    def user_agent
+      unless @user_agent
+        @user_agent = WWW::Mechanize.new
+        @user_agent.basic_auth(@user, @password)
+      end
+
+      @user_agent
+    end
+
   end
 end
